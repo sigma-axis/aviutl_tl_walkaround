02cf076b0799e7550a980598487b778580c0d851
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2024-08-03 08:35:56 +0900

    v1.24

--- tl_walkaround.cpp
+++ tl_walkaround.cpp
@@ -1367,7 +1367,7 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD fdwReason, LPVOID lpvReserved)
 // 看板．
 ////////////////////////////////
 #define PLUGIN_NAME		"TLショトカ移動"
-#define PLUGIN_VERSION	"v1.24-beta1"
+#define PLUGIN_VERSION	"v1.24"
 #define PLUGIN_AUTHOR	"sigma-axis"
 #define PLUGIN_INFO_FMT(name, ver, author)	(name##" "##ver##" by "##author)
 #define PLUGIN_INFO		PLUGIN_INFO_FMT(PLUGIN_NAME, PLUGIN_VERSION, PLUGIN_AUTHOR)
